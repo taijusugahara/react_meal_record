9703e4d990abc1df018f1f9dd6eaeb588ef61bfb
taijusugahara <96ayutuki@gmail.com>
2022-07-05 22:03:14 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Redux
+            ReduxOk
           </a>
           <span>, </span>
           <a
